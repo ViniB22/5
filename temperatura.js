3b846a87340318494353363cab49e456e3894e1b
ViniB22 <146386108+ViniB22@users.noreply.github.com>
2024-02-07 17:02:02 -0300

    Ultima versão definitiva

--- temperatura.js
+++ temperatura.js
@@ -3,5 +3,5 @@ let resposta = document.getElementById('resposta')
 function calcular(){
     let cel = Number(document.getElementById('cel').value)
     let calcula = (cel * 9 / 5) + 32
-    resposta.innerHTML = "Isso da " + calcula + " gigraus Fahrenheit"
+    resposta.innerHTML = "Isso da " + calcula + " graus Fahrenheit"
 }
